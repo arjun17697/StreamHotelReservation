5dcab303dace543ae9a682106aa25c7f29e2f02c
arjun17697 <arjun17697@gmail.com>
2020-10-05 21:40:37 +0530

    Add:UC1 Added hotel name and weekday rates for regular customer

--- src/main/HotelReservation.java
+++ src/main/HotelReservation.java
@@ -1,8 +1,14 @@
 package main;
 
+import main.hotelDetails;
+
 public class HotelReservation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Hotel Reservation Program");
-
 	}
+
+	hotelDetails lakewood = new hotelDetails("lakewood", 110);
+	hotelDetails bridge = new hotelDetails("Bridgewood", 160);
+	hotelDetails ridge = new hotelDetails("Ridgewood", 220);
+
 }
---
+++ src/main/hotelDetails.java
@@ -0,0 +1,13 @@
+package main;
+
+public class hotelDetails {
+	String hotelName;
+	int weekday_regular;
+	public hotelDetails(String hotelName, int weekday_regular) {
+		
+		this.hotelName = hotelName;
+		this.weekday_regular = weekday_regular;
+	}
+	
+
+}
